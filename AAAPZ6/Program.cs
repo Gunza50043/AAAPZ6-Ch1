b86d8030c69d758b76c277d77568b701c6803f6f
Добрый Александр <saschaomel4uk@yandex.ru>
2023-04-03 10:47:44 +0700

    Добавьте файлы проекта.

---
+++ AAAPZ6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Serialization;
+using System.Linq;
+
+namespace AAAPZ6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Students students = new Students();
+            var atestList = new List<Students>
+            {
+                new Students{student= "Петров", predmet = "математика", semestr = 1, ocenca = "Плохо"},
+                new Students{student= "Иванов", predmet = "математика", semestr = 1, ocenca = "Хорошо"},
+                new Students{student= "Сидоров", predmet = "математика", semestr = 1, ocenca = "Отлично"},
+                new Students{student= "Никифоров", predmet = "математика", semestr = 1, ocenca = "Плохо"}
+
+            };
+
+            Console.WriteLine("Хорошие Студенты: ");
+            students.PrintStud(atestList);
+            Console.WriteLine("Да на кол их..: ");
+            students.SavePloxieStud(atestList);
+        }
+
+        
+    }
+}
---
+++ AAAPZ6/Students.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAAPZ6
+{
+    public class Students
+    {
+        public string student;
+        public int semestr;
+        public string predmet;
+        public string ocenca;
+
+        public void PrintStud(List<Students> atestLists)
+        {
+            var atestStusent = atestLists.Where(at => at.ocenca == "Хорошо" || at.ocenca == "Отлично")
+                                         .Select(at => at.student +" " + at.ocenca)
+                                         .ToList();
+            atestStusent.ForEach(at => Console.WriteLine(at));
+        }
+
+        public void SavePloxieStud(List<Students> atestLists)
+        {
+            var atestStusent = atestLists.Where(at => at.ocenca == "Плохо")
+                             .Select(at => at.student + " " + at.ocenca)
+                             .ToList();
+            //atestStusent.ForEach(at => Console.WriteLine(at));
+            List<string> ploxieList = atestStusent.ToList();
+            ploxieList.ForEach(at => Console.WriteLine(at));
+
+        }
+
+
+    }
+
+}
